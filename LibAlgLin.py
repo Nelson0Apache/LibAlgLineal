7405cc6dd00f26b83f9791a5768ad90b2df77e3a
Nelson Apache <molina123apache@gmail.com>
2022-05-09 18:38:16 -0500

    agregue nueva funcion de suma de matrices y modifique la la funcion de matriz cuadrada

--- LibAlgLin.py
+++ LibAlgLin.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 
 
@@ -9,24 +10,50 @@ def matrizCuadrada():
     funcionará si el usuario escribe un str en vez de un float o un int
     """
     try:
-        cuad = eval(input("Ingrese el tamaño que quiere para su matriz cuadrada: "))
-        matriz = []
-        for i in range(cuad):
-            i = i + 1
-            fila = []
-            for j in range(cuad):
-                j += 1
-                numeros = (eval(input("Introducir los componentes de la matriz ({}, {}): " .format(i , j)))) 
-                fila.append(numeros)
-                matriz.append(fila)
+        cuad = int(input("Ingrese el tamaño que quiere para su matriz cuadrada: "))
     except:
-        print("Ingreso un str y el codigo solo sirve para numeros ya sean int o float")  
+        print("Ingrese un numero entero")
     else:
-        print(matriz)  
-    finally:
-        print("Fin :D")        
+        try:
+            matriz = []
+            for i in range(cuad):
+                i = i + 1
+                fila = []
+                for j in range(cuad):
+                    j += 1
+                    numeros = (eval(input("Introducir los componentes de la matriz ({}, {}): " .format(i , j)))) 
+                    fila.append(numeros)
+                    matriz.append(fila)
+        except:
+            print("Ingreso un str y el codigo solo sirve para numeros ya sean int o float")  
+    return(matriz)  
+         
 
-print(matrizCuadrada())
-            
+def sumarMatrizNxN(Ma1, Ma2):
+    """
+    Toma dos matrices como entrada, primero verifica si tienen la misma dimesion, luego si la matriz es cuadrada y si es así devuelve 
+    la suma de las dos matrices.
+    """
+    if len(Ma1) != len(Ma2):
+        raise ArithmeticError("No tienen la misma cantidad de fila")
+    
+    if len(Ma1[0]) != len(Ma2[0]):
+        raise ArithmeticError("No tienen la misma cantidad de columna")
+    
+    if len(Ma1) == len(Ma1[0]):
+
+        sumarMatriz = []
+        for i in range(len(Ma1)):
+            fila = []
+            for j in range(len(Ma1[0])):
+                fila.append(Ma1[i][j] + Ma2[i][j])
+            sumarMatriz.append(fila)              
+    return(sumarMatriz)
+      
+Ma1 = [[1, 2], [3, 4]]
+Ma2 = [[4, 8], [9, 5]]
+
+print(sumarMatrizNxN(Ma1, Ma2))
+
 
 
